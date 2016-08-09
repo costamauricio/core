20958cffae9a63700de23a273b5aed6b38e87162
Maurício Costa <mauriciocosta.cm@gmail.com>
2016-08-09 19:51:30 -0300

    Adicionando testes do comando login

---
+++ test/Command/loginTest.js
@@ -0,0 +1,26 @@
+var expect = require("chai").expect;
+var path = require("path");
+
+var cvs = require("../../CVS");
+
+describe("Login", function() {
+
+  describe("Valid User", function() {
+
+    it ("Login with password", function(done) {
+
+      var pserver = ":pserver:cvsuser@localhost:/var/cvsroot",
+          repo = new cvs(pserver);
+
+      repo.login("").then(function() {
+        return done();
+      }).catch(done)
+
+    })
+
+    it ("Login without password", function(done) {
+    
+    })
+  })
+
+});
